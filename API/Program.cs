ba2d6bba4f6393596b654b2d260442eb185e632e
Sayak Kundu <withsayak101@gmail.com>
2024-06-21 01:19:57 +0530

    Section - 3

--- API/Program.cs
+++ API/Program.cs
@@ -7,10 +7,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4201"));
 app.MapControllers();
 
 app.Run();
