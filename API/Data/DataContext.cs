480add20176e5ec094dc7503e49578af1f6dd2a9
Sayak Kundu <withsayak101@gmail.com>
2024-06-20 23:31:17 +0530

    Section - 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext(DbContextOptions dbContextOptions) : DbContext(dbContextOptions)
+    {
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
